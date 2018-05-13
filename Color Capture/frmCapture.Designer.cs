255ec9c3989f58008171b09ddb2421e97c1682ac
Ekapop <eploentham@gmail.com>
2018-05-13 08:27:38 +0700

    1
    1

---
+++ Color Capture/frmCapture.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Color_Capture
+{
+    partial class frmCapture
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCapture));
+            this.txtHtmlColor = new System.Windows.Forms.TextBox();
+            this.ntfColorCapture = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tblColors = new System.Windows.Forms.TableLayoutPanel();
+            this.picSelColor = new System.Windows.Forms.PictureBox();
+            this.picBtnShowHide = new System.Windows.Forms.PictureBox();
+            this.picBtnCopy = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picSelColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnShowHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnCopy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHtmlColor
+            // 
+            this.txtHtmlColor.HideSelection = false;
+            this.txtHtmlColor.Location = new System.Drawing.Point(75, 15);
+            this.txtHtmlColor.Name = "txtHtmlColor";
+            this.txtHtmlColor.ReadOnly = true;
+            this.txtHtmlColor.Size = new System.Drawing.Size(148, 20);
+            this.txtHtmlColor.TabIndex = 1;
+            this.txtHtmlColor.TabStop = false;
+            // 
+            // ntfColorCapture
+            // 
+            this.ntfColorCapture.Icon = ((System.Drawing.Icon)(resources.GetObject("ntfColorCapture.Icon")));
+            this.ntfColorCapture.Text = "Color Capture";
+            this.ntfColorCapture.Visible = true;
+            this.ntfColorCapture.Click += new System.EventHandler(this.ntfColorCapture_Click);
+            this.ntfColorCapture.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ntfColorCapture_MouseDoubleClick);
+            // 
+            // tblColors
+            // 
+            this.tblColors.AutoScroll = true;
+            this.tblColors.ColumnCount = 4;
+            this.tblColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tblColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tblColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tblColors.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tblColors.Location = new System.Drawing.Point(15, 51);
+            this.tblColors.Name = "tblColors";
+            this.tblColors.RowCount = 1;
+            this.tblColors.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblColors.Size = new System.Drawing.Size(267, 190);
+            this.tblColors.TabIndex = 6;
+            // 
+            // picSelColor
+            // 
+            this.picSelColor.BackColor = System.Drawing.Color.White;
+            this.picSelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSelColor.Location = new System.Drawing.Point(48, 15);
+            this.picSelColor.Name = "picSelColor";
+            this.picSelColor.Size = new System.Drawing.Size(21, 20);
+            this.picSelColor.TabIndex = 7;
+            this.picSelColor.TabStop = false;
+            // 
+            // picBtnShowHide
+            // 
+            this.picBtnShowHide.Image = global::Color_Capture.Properties.Resources.down1;
+            this.picBtnShowHide.Location = new System.Drawing.Point(251, 15);
+            this.picBtnShowHide.Name = "picBtnShowHide";
+            this.picBtnShowHide.Size = new System.Drawing.Size(21, 20);
+            this.picBtnShowHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBtnShowHide.TabIndex = 5;
+            this.picBtnShowHide.TabStop = false;
+            this.picBtnShowHide.Click += new System.EventHandler(this.picBtnShowHide_Click);
+            this.picBtnShowHide.MouseHover += new System.EventHandler(this.picBtnShowHide_MouseHover);
+            // 
+            // picBtnCopy
+            // 
+            this.picBtnCopy.Image = global::Color_Capture.Properties.Resources.copy1_32;
+            this.picBtnCopy.Location = new System.Drawing.Point(228, 15);
+            this.picBtnCopy.Name = "picBtnCopy";
+            this.picBtnCopy.Size = new System.Drawing.Size(21, 20);
+            this.picBtnCopy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBtnCopy.TabIndex = 4;
+            this.picBtnCopy.TabStop = false;
+            this.picBtnCopy.Click += new System.EventHandler(this.picBtnCopy_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.BackgroundImage = global::Color_Capture.Properties.Resources.drop6;
+            this.btnCapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCapture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCapture.Location = new System.Drawing.Point(12, 15);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(24, 20);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // frmCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 46);
+            this.Controls.Add(this.picSelColor);
+            this.Controls.Add(this.tblColors);
+            this.Controls.Add(this.picBtnShowHide);
+            this.Controls.Add(this.picBtnCopy);
+            this.Controls.Add(this.txtHtmlColor);
+            this.Controls.Add(this.btnCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCapture";
+            this.Text = "Color Capture";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmCapture_Load);
+            this.Resize += new System.EventHandler(this.frmCapture_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.picSelColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnShowHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBtnCopy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.TextBox txtHtmlColor;
+        private System.Windows.Forms.NotifyIcon ntfColorCapture;
+        private System.Windows.Forms.PictureBox picBtnCopy;
+        private System.Windows.Forms.PictureBox picBtnShowHide;
+        private System.Windows.Forms.TableLayoutPanel tblColors;
+        private System.Windows.Forms.PictureBox picSelColor;
+    }
+}
+
---
+++ Color Capture/frmCapture.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Capture
+{
+    public partial class frmCapture : Form
+    {
+        private List<ColorControls> colorControlsList;
+        public frmCapture()
+        {
+            InitializeComponent();
+            colorControlsList = new List<ColorControls>();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            MouseHook.Start();
+            MouseHook.MouseAction += MouseHook_MouseAction;
+        }
+
+        private void MouseHook_MouseAction(object sender, MouseHook.RawMouseEventArgs e)
+        {
+            Console.WriteLine("mouse act" + e.Message);
+            Color clcolor = GetColorAt(e.Point.x, e.Point.y);
+            picSelColor.BackColor = clcolor;
+            string htmlcol = ColorTranslator.ToHtml(Color.FromArgb(clcolor.ToArgb()));
+            txtHtmlColor.Text = htmlcol;
+            if (e.Message == MouseHook.MouseMessages.WM_LBUTTONDOWN)
+            {
+                Console.WriteLine("mouse click " + e.Point.x + ":" + e.Point.y);
+                MouseHook.Stop();
+                MouseHook.MouseAction -= MouseHook_MouseAction;
+                addColor(clcolor);
+            }
+        }
+        private void addColor(Color selcol)
+        {
+            var ncc = new ColorControls(selcol, colorControlsList.Count);
+            ncc.DeleteClick += Ncc_DeleteClick;
+            colorControlsList.Add(ncc);
+            drawTable();
+        }
+        private void drawTable()
+        {
+            int row = 0;
+            SuspendLayout();
+            tblColors.Controls.Clear();
+            tblColors.RowCount = colorControlsList.Count;
+            for (int i = colorControlsList.Count - 1; i >= 0; i--)
+            {
+                var cc = colorControlsList[i];
+                tblColors.Controls.Add(cc.picColor, 0, row);
+                tblColors.Controls.Add(cc.htmlColor, 1, row);
+                tblColors.Controls.Add(cc.picCopy, 2, row);
+                tblColors.Controls.Add(cc.picDelete, 3, row);
+                cc.Refresh();
+                row++;
+            }
+
+            tblColors.Refresh();
+
+            ResumeLayout();
+
+            Refresh();
+        }
+        private void Ncc_DeleteClick(object sender, EventArgs e)
+        {
+            var cc = (ColorControls)sender;
+            colorControlsList.Remove(cc);
+            drawTable();
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+
+        private void ntfColorCapture_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ntfColorCapture_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        private void frmCapture_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                ntfColorCapture.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                ntfColorCapture.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+        private void picBtnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtHtmlColor.Text);
+        }
+        internal class ColorControls
+        {
+            public PictureBox picColor { get; set; }
+            public TextBox htmlColor { get; set; }
+            public PictureBox picCopy { get; set; }
+            public PictureBox picDelete { get; set; }
+            public int Idx { get; set; }
+            public ColorControls() { }
+            public event EventHandler DeleteClick = delegate { };
+            public ColorControls(Color htmlcolor, int idx)
+            {
+                Idx = idx;
+                picColor = new PictureBox() { Name = "picSelColor" + idx.ToString(), Size = new Size(20, 20), BackColor = htmlcolor,BorderStyle=BorderStyle.FixedSingle };
+                picCopy = new PictureBox() { Name = "picBtnCopy" + idx.ToString(), Size = new Size(20, 20), SizeMode = PictureBoxSizeMode.Zoom, Image = Properties.Resources.copy1_32 };
+                picDelete = new PictureBox() { Name = "picBtnDelete" + idx.ToString(), Size = new Size(20, 20), SizeMode = PictureBoxSizeMode.Zoom, Image = Properties.Resources.delete121_32 };
+                htmlColor = new TextBox() { Name = "txtHtmlColor" + idx.ToString(), Size = new Size(148, 20), Text = ColorTranslator.ToHtml(Color.FromArgb(htmlcolor.ToArgb())), ReadOnly = true };
+                picCopy.Click += PicCopy_Click;
+                picDelete.Click += PicDelete_Click;
+            }
+
+            private void PicDelete_Click(object sender, EventArgs e)
+            {
+                DeleteClick(this, new EventArgs());
+            }
+
+            private void PicCopy_Click(object sender, EventArgs e)
+            {
+                Clipboard.SetText(htmlColor.Text);
+            }
+
+            public void Refresh()
+            {
+
+                picColor.Refresh();
+                picColor.Update();
+                picCopy.Update();
+                picCopy.Refresh();
+                picDelete.Refresh();
+                picCopy.Update();
+            }
+        }
+        private bool frmExtended = false;
+        private void picBtnShowHide_Click(object sender, EventArgs e)
+        {
+            int maxHeight = 300;
+            int minHeight = 85;
+            if (!frmExtended)
+            {
+                picBtnShowHide.Image = Properties.Resources.up1;
+                this.Height = maxHeight;
+                frmExtended = true;
+            }else
+            {
+                picBtnShowHide.Image = Properties.Resources.down1;
+                this.Height = minHeight;
+                frmExtended = false;
+            }
+        }
+
+        private void picBtnShowHide_MouseHover(object sender, EventArgs e)
+        {
+            if (!frmExtended) picBtnShowHide.Image = Properties.Resources.down2;
+            else picBtnShowHide.Image = Properties.Resources.up2;
+        }
+
+        private void frmCapture_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
